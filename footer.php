3867412cdd46f3deb3c6dc88dbc6fda2430123cc
Ricardo Gava <Ricardoagava@gmail.com>
2020-03-31 03:55:57 -0300

    All the footer area is now editable

--- footer.php
+++ footer.php
@@ -1,7 +1,16 @@
+<?php $contact = get_page_by_title('contato'); ?>
+
+<style>
+    .break {
+        background: url("<?php the_field('break_bg', $contact); ?>") no-repeat center;
+        background-size: cover;
+    }
+</style>
+
 <section class="break">
     <blockquote class="quote-externo container">
-        <p>"O verdadeiro segredo da felicidade está em ter um genuíno interesse por todos os detalhes da vida cotidiana."</p>
-        <cite>WILLIAM MORRIS</cite>
+        <?php the_field('footer_phrase', $contact); ?>
+        <cite><?php the_field('footer_author', $contact); ?></cite>
     </blockquote>
 </section>
 
@@ -10,16 +19,14 @@
         <div class="container">
             <div class="grid-8 footer-history">
                 <h3>Nossa História</h3>
-                <p>O verdadeiro segredo da felicidade está em ter um genuíno interesse por todos os detalhes da vida cotidiana cotidiana.
-                    Interesse por todos os detalhes da vida cotidiana cotidiana.
-                </p>
+                <?php the_field('history_resume', $contact); ?>
             </div>
             <div class="grid-4 footer-contact">
                 <h3>Contato</h3>
                 <ul>
-                    <li>- 21 23232-4343</li>
-                    <li>- contato@bikcraft.com</li>
-                    <li>- Botafogo - RJ</li>
+                    <li>- <?php the_field('telephone', $contact); ?></li>
+                    <li>- <?php the_field('email', $contact); ?></li>
+                    <li>- <?php the_field('address_1', $contact); ?></li>
                 </ul>
             </div>
             <div class="grid-4 footer-social">
@@ -30,7 +37,7 @@
     </div>
     <div class="copy">
         <div class="container">
-            <p class="grid-16">Bikcraft 2019 - Alguns direitos reservados.</p>
+            <p class="grid-16"><?php bloginfo('name'); ?> <?php echo date('Y'); ?> - Alguns direitos reservados.</p>
         </div>
     </div>
 </footer>
